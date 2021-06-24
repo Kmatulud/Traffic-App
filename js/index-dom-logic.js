fd9fbfb12fd6428b242535c97876bdf6b6fc9ca0
kmatulud <kmatulud@student.wethinkcode.co.za>
2021-06-24 15:07:51 +0200

    Fix errors

--- js/index-dom-logic.js
+++ js/index-dom-logic.js
@@ -1,31 +1,67 @@
-var usernameElem = document.querySelector('#user');
-var routesElem = document.querySelector('#options');
+var usernameElem = document.querySelector("#user");
+var routesElem = document.querySelector("#options");
 var timeElem = document.querySelector("#time");
-var dateElem = document.querySelector('#day');
-var buttonElem = document.querySelector('.button');
-var clearBtn = document.querySelector('.clear');
+var dateElem = document.querySelector("#day");
+var buttonElem = document.querySelector(".button");
+var clearBtn = document.querySelector(".clear");
 
 var array = localStorage.getItem("arr") || [];
-buttonElem.addEventListener("click", function(e){
 
-    e.preventDefault();
-    var day = new Date(dateElem.value).getDay();
-    var daysOFWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+buttonElem.addEventListener("click", function (e) {
+	var newArray = [];
 
-    var each = routesElem.options[routesElem.selectedIndex].value;
-    array.push(usernameElem.value);
-    array.push(each);
-    array.push(timeElem.value);
-    array.push(dateElem.value);
-    array.push(daysOFWeek[day]);
-    localStorage.setItem("arr", array);
-    localStorage.getItem("arr");
+	e.preventDefault();
+	var day = new Date(dateElem.value).getDay();
+	var daysOfWeek = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
 
-    usernameElem.value = "";
+	var each = routesElem.options[routesElem.selectedIndex].value;
 
-    console.log(each, usernameElem.value, timeElem.value, dateElem.value);
-})
-clearBtn.addEventListener("click", function(){
-    localStorage.clear();
-    location.reload();
-})
+	if (localStorage.getItem("strObj") !== null) {
+		var local = JSON.parse(localStorage.getItem("strObj"));
+		local.push({ Name: usernameElem.value });
+		local.push({ Route: routesElem.value });
+		local.push({ Duration: timeElem.value });
+		local.push({ Date: dateElem.value });
+		local.push({ Day: daysOfWeek[day] });
+
+		JSON.stringify(local);
+		var strObj = JSON.stringify(local);
+		localStorage.setItem("strObj", strObj);
+		var newArray = ([].length = 0);
+	} else {
+		newArray.push({ Name: usernameElem.value });
+		newArray.push({ Route: routesElem.value });
+		newArray.push({ Duration: timeElem.value });
+		newArray.push({ Date: dateElem.value });
+		newArray.push({ Day: daysOfWeek[day] });
+
+		JSON.stringify(newArray);
+		var strObj = JSON.stringify(newArray);
+		localStorage.setItem("strObj", strObj);
+		var newArray = ([].length = 0);
+	}
+
+	array.push(usernameElem.value);
+	array.push(each);
+	array.push(timeElem.value);
+	array.push(dateElem.value);
+	array.push(daysOfWeek[day]);
+	localStorage.setItem("arr", array);
+	localStorage.getItem("arr");
+
+	usernameElem.value = "";
+
+	// console.log(each, usernameElem.value, timeElem.value, dateElem.value);
+});
+clearBtn.addEventListener("click", function () {
+	localStorage.clear();
+	location.reload();
+});
