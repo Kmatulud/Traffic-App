8fd200f1aa16e18705c7fc6980843021365f6e9a
Mthobisi-za <mthobisimtho333@gmail.com>
2021-06-24 21:27:31 +0200

    done page

--- js/index-dom-logic.js
+++ js/index-dom-logic.js
@@ -4,65 +4,70 @@ var timeElem = document.querySelector("#time");
 var dateElem = document.querySelector("#day");
 var buttonElem = document.querySelector(".button");
 var clearBtn = document.querySelector(".clear");
+//elemenyts
 
-var array = localStorage.getItem("arr") || [];
-var data;
-buttonElem.addEventListener("click", function (e) {
-	var newArray = [];
+buttonElem.addEventListener("click", function(e) {
+    var newArray = [];
+    ////global variables
+    var day = new Date(dateElem.value).getDay();
+    var daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    ///list for week
+    var each = routesElem.options[routesElem.selectedIndex].value;
 
-	e.preventDefault();
-	var day = new Date(dateElem.value).getDay();
-	var daysOfWeek = [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday",
-	];
+    if (localStorage.getItem("strObj") !== null) {
+        var local = JSON.parse(localStorage.getItem("strObj"));
+        var full = {
+                name: usernameElem.value,
+                Route: routesElem.value,
+                Duration: timeElem.value,
+                Date: dateElem.value,
+                Day: daysOfWeek[day],
+            }
+            // local.push({ Name: usernameElem.value });
+            // local.push({ Route: routesElem.value });
+            // local.push({ Duration: timeElem.value });
+            // local.push({ Date: dateElem.value });
+            // local.push({ Day: daysOfWeek[day] });
+            // ////adding id to our obj
+            // local.push({ id: day });
+        local.push(full)
+        JSON.stringify(local);
+        var strObj = JSON.stringify(local);
+        localStorage.setItem("strObj", strObj);
+        var newArray = ([].length = 0);
+    } else {
+        // newArray.push({ Name: usernameElem.value });
+        // newArray.push({ Route: routesElem.value });
+        // newArray.push({ Duration: timeElem.value });
+        // newArray.push({ Date: dateElem.value });
+        // newArray.push({ Day: daysOfWeek[day] });
+        // newArray.push({ id: day });
+        var full = {
+            name: usernameElem.value,
+            Route: routesElem.value,
+            Duration: timeElem.value,
+            Date: dateElem.value,
+            Day: daysOfWeek[day],
+        }
+        newArray.push(full)
+        JSON.stringify(newArray);
+        var strObj = JSON.stringify(newArray);
+        localStorage.setItem("strObj", strObj);
+        var newArray = ([].length = 0);
+    }
 
-	var each = routesElem.options[routesElem.selectedIndex].value;
-
-	if (localStorage.getItem("strObj") !== null) {
-		var local = JSON.parse(localStorage.getItem("strObj"));
-		local.push({ Name: usernameElem.value });
-		local.push({ Route: routesElem.value });
-		local.push({ Duration: timeElem.value });
-		local.push({ Date: dateElem.value });
-		local.push({ Day: daysOfWeek[day] });
-
-		JSON.stringify(local);
-		var strObj = JSON.stringify(local);
-		localStorage.setItem("strObj", strObj);
-		var newArray = ([].length = 0);
-	} else {
-		newArray.push({ Name: usernameElem.value });
-		newArray.push({ Route: routesElem.value });
-		newArray.push({ Duration: timeElem.value });
-		newArray.push({ Date: dateElem.value });
-		newArray.push({ Day: daysOfWeek[day] });
-
-		JSON.stringify(newArray);
-		var strObj = JSON.stringify(newArray);
-		localStorage.setItem("strObj", strObj);
-		var newArray = ([].length = 0);
-	}
-
-	array.push(usernameElem.value);
-	array.push(each);
-	array.push(timeElem.value);
-	array.push(dateElem.value);
-	array.push(daysOfWeek[day]);
-	localStorage.setItem("arr", array);
-	localStorage.getItem("arr");
-
-	usernameElem.value = "";
-    data = JSON.parse(localStorage.getItem("strObj"));
-    
-	// console.log(each, usernameElem.value, timeElem.value, dateElem.value);
+    usernameElem.value = "";
+    // console.log(each, usernameElem.value, timeElem.value, dateElem.value);
 });
-clearBtn.addEventListener("click", function () {
-	localStorage.clear();
-	location.reload();
+clearBtn.addEventListener("click", function() {
+    localStorage.clear();
+    location.reload();
 });
--- js/screen2.js
+++ js/screen2.js
@@ -1,1 +1,11 @@
-console.log(data);
+function lastIndex() {
+    var data = JSON.parse(localStorage.getItem("strObj"));
+    var last = data[data.length - 1]
+    var span1 = document.querySelector(".name");
+    var routetT = document.querySelector(".route");
+    var dura = document.querySelector(".duration");
+    span1.innerHTML = last.name
+    routetT.innerHTML = last.Route
+    dura.innerHTML = last.Duration
+}
+lastIndex()
