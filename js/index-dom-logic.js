9da8cb9a3a753d769e94e553773a8c79c2e6c539
kmatulud <kmatulud@student.wethinkcode.co.za>
2021-06-24 13:28:33 +0200

    edits

--- js/index-dom-logic.js
+++ js/index-dom-logic.js
@@ -1,22 +1,29 @@
 var usernameElem = document.querySelector('#user');
-var locationElem = document.querySelector('#location');
-var destinationElem = document.querySelector('#destination');
+var routesElem = document.querySelector('#options');
+var timeElem = document.querySelector("#time");
 var dateElem = document.querySelector('#day');
 var buttonElem = document.querySelector('.button');
 var clearBtn = document.querySelector('.clear');
 
-buttonElem.addEventListener("click", function(){
-    var array = localStorage.getItem("arr") || [];
+var array = localStorage.getItem("arr") || [];
+buttonElem.addEventListener("click", function(e){
+
+    e.preventDefault();
+    var day = new Date(dateElem.value).getDay();
+    var daysOFWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    var each = routesElem.options[routesElem.selectedIndex].value;
     array.push(usernameElem.value);
-    array.push(locationElem.value);
-    array.push(destinationElem.value);
+    array.push(each);
+    array.push(timeElem.value);
     array.push(dateElem.value);
+    array.push(daysOFWeek[day]);
     localStorage.setItem("arr", array);
     localStorage.getItem("arr");
 
     usernameElem.value = "";
-    destinationElem.value = "";
-    locationElem.value = "";
+
+    console.log(each, usernameElem.value, timeElem.value, dateElem.value);
 })
 clearBtn.addEventListener("click", function(){
     localStorage.clear();
