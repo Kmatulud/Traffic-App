db9e54cf3fac516091076d25e2c85d69cf95e6ce
Mthobisi-za <mthobisimtho333@gmail.com>
2021-06-25 08:24:06 +0200

    updated data

--- js/screen3.js
+++ js/screen3.js
@@ -1,26 +1,46 @@
 var labels = []
 var datab = []
+var map = {}
 var data = JSON.parse(localStorage.getItem("strObj"));
 data.forEach(val => {
-    labels.push(val.Day)
-    datab.push(val.Duration)
+    //   labels.push(val.Day)
+    //   datab.push(val.Duration)
+    ////
+    if (map.hasOwnProperty(val.Day)) {
+
+    } else {
+        map[val.Day] = 0
+    }
 })
-console.log(labels, data)
-    ////-----
+data.forEach(val => {
+    ////
+    for (const key in map) {
+        if (key == val.Day) {
+            var duration = parseFloat(val.Duration)
+            map[key] = map[key] + duration
+        }
+    }
+});
+for (const key in map) {
+    labels.push(key)
+    datab.push(map[key])
+}
+
+
+////-----
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: labels,
         datasets: [{
-            label: 'Daily Trip Duration',
+            label: 'Daily Trip Duration (minutes)',
             data: datab,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)'
-
             ],
             borderWidth: 1
         }]
