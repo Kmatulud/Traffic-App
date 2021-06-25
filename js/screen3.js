ed1ca92d4e2f375ef6c0c7db4fc2f8bf4a9cfc97
Mthobisi-za <mthobisimtho333@gmail.com>
2021-06-25 11:33:59 +0200

    map

--- js/screen3.js
+++ js/screen3.js
@@ -1,45 +1,71 @@
-var labels = []
-var datab = []
-var map = {}
-var data = JSON.parse(localStorage.getItem("strObj"));
-data.forEach(val => {
-    //   labels.push(val.Day)
-    //   datab.push(val.Duration)
-    ////
-    if (map.hasOwnProperty(val.Route)) {
+// var labels = []
+// var datab = []
+// var map = {}
+// var data = JSON.parse(localStorage.getItem("strObj"));
+// data.forEach(val => {
+//     //   labels.push(val.Day)
+//     //   datab.push(val.Duration)
+//     ////
+//     if (map.hasOwnProperty(val.Route)) {
 
-    } else {
-        if (undefined == val.Route) {
-            console.log("undefined")
-        } else {
-            map[val.Route] = 0;
-        }
+//     } else {
+//         if (undefined == val.Route) {
+//             console.log("undefined")
+//         } else {
+//             map[val.Route] = 0;
+//         }
+//     }
+// });
+// data.forEach(val => {
+//     ////
+//     for (const key in map) {
+//         if (key == val.Route) {
+//             var duration = parseFloat(val.Duration)
+//             map[key] = map[key] + duration
+//         }
+//     }
+// });
+// for (const key in map) {
+//     labels.push(key)
+//     datab.push(map[key])
+// }
+
+////-----map data
+const list = JSON.parse(localStorage['strObj']);
+const durationsForRoutes = list.reduce((map, routeData) => {
+    if (!map[routeData.Route]) {
+        map[routeData.Route] = [];
     }
-});
-data.forEach(val => {
-    ////
-    for (const key in map) {
-        if (key == val.Route) {
-            var duration = parseFloat(val.Duration)
-            map[key] = map[key] + duration
+    map[routeData.Route].push(Number(routeData.Duration));
+    return map;
+}, {});
+let routeAverages = Object
+    .keys(durationsForRoutes)
+    .map(key => {
+        const durations = durationsForRoutes[key];
+        const total = durations.reduce((totalDuration, duration) => {
+            totalDuration += duration;
+            return totalDuration;
+        }, 0)
+        const average = total / durations.length
+        return {
+            route: key,
+            average: Number(average.toFixed(2))
         }
-    }
-});
-for (const key in map) {
-    labels.push(key)
-    datab.push(map[key])
-}
-
-
-////-----
+    });
+const keys = routeAverages.map(routeData => routeData.route);
+const totals = routeAverages.map(routeData => routeData.average);
+console.log(keys, totals)
+    // plug  keys and totals into your graph...
+    ////-----
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: labels,
+        labels: keys,
         datasets: [{
             label: 'Daily Trip Duration (minutes)',
-            data: datab,
+            data: totals,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)'
             ],
